4c72962b38fb0526788aeb99eacdb82af2926d0a
sas4211 <sas4211@gmail.com>
2024-04-25 23:03:10 +0400

    currency-converter

---
+++ index.js
@@ -0,0 +1,40 @@
+import inquirer from 'inquirer';
+const Currency = {
+    USD: 1,
+    EUR: 0.9,
+    GDP: 0.79,
+    AE: 3.75,
+    INR: 74.59,
+    PKR: 280
+};
+let userAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        type: "list",
+        message: "Enter from Currency",
+        choices: ["USD", "EUR", "GDP", "AE", "INR", "PKR"
+        ]
+    },
+    {
+        name: "to",
+        type: "list",
+        message: "Enter To Currency",
+        choices: ["USD", "EUR", "GDP", "AE", "INR", "PKR"
+        ]
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter Your Amount",
+    }
+]);
+console.log(userAnswer);
+let fromAnswer = Currency[userAnswer.from];
+let toAnswer = Currency[userAnswer.to];
+let amountAns = userAnswer.amount;
+console.log(fromAnswer);
+console.log(toAnswer);
+console.log(amountAns);
+let baseAmount = amountAns / fromAnswer;
+let convertAmount = baseAmount * toAnswer;
+console.log(convertAmount);
